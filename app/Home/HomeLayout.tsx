87905812e646fb3e7ccef182fc010222eb5aa6bf
HyeontaeKim0 <hyeontae.kim@wincl.io>
2026-01-15 09:17:03 +0900

    fix : tailwind ver4와 적용하려던 hero ui와 충돌 수정

--- app/Home/HomeLayout.tsx
+++ app/Home/HomeLayout.tsx
@@ -1,9 +1,9 @@
 import { signOut } from "../Auth/Auth";
 import { Session } from "next-auth";
+
 export function HomeLayout({ session }: { session: Session }) {
   return (
     <div>
-      <p>환영합니다, {session.user?.name}님!</p>
       <img src={session.user?.image ?? ""} alt="Profile" />
       <form
         action={async () => {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
 
 export const metadata: Metadata = {
   title: "UnTitled Community",
